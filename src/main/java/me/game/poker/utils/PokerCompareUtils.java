f1f7a33a9162bf693c40eeb9cd201f4b6ab0a42e
Mark <mamian521@gmail.com>
2018-04-09 22:22:06 +0800

    add PokerUtils class

--- src/main/java/me/game/poker/utils/PokerCompareUtils.java
+++ src/main/java/me/game/poker/utils/PokerCompareUtils.java
@@ -1,4 +1,7 @@
 package me.game.poker.utils;
 
+/**
+ * 牌型大小比较工具类
+ */
 public class PokerCompareUtils {
 }
--- src/main/java/me/game/poker/utils/PokerTypeUtils.java
+++ src/main/java/me/game/poker/utils/PokerTypeUtils.java
@@ -1,4 +1,8 @@
 package me.game.poker.utils;
 
+/**
+ * 牌型工具类
+ * 该类负责定义牌型及判断牌型
+ */
 public class PokerTypeUtils {
 }
