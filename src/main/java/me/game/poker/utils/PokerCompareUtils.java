da91cb7668fc78dbbc1199890e74798403abd0e7
yangkaile <guyexing@foxmail.com>
2018-05-29 22:16:15 +0800

    添加联机模式下牌型大小比较的方法

--- src/main/java/me/game/poker/utils/PokerCompareUtils.java
+++ src/main/java/me/game/poker/utils/PokerCompareUtils.java
@@ -1,7 +1,47 @@
 package me.game.poker.utils;
 
+import me.game.poker.entity.Poker;
+import me.game.poker.entity.PokerType;
+
+import java.util.List;
+
 /**
  * 牌型大小比较工具类
  */
 public class PokerCompareUtils {
+    /**
+     * 比较两手牌的大小
+     * a b ,当a大于b时返回true
+     * 王炸通吃
+     * 炸弹仅次于王炸
+     * 其他牌必须牌型相等才能比较
+     */
+    public static boolean comparePukers(List<Poker> a, List<Poker> b){
+        a.sort(new PokerComparatorDesc());
+        b.sort(new PokerComparatorDesc());
+        if(a.size() < 1 || b.size() < 1) return false;//空
+        if(PokerTypeUtils.isKingBoom(b) != -1) return false;//b是王炸
+        if(PokerTypeUtils.isKingBoom(a) != -1) return true;//a是王炸
+        if(PokerTypeUtils.isBoom(b) != -1){//b是炸弹
+            if(PokerTypeUtils.isBoom(a) != -1) return compareOne(a.get(0),b.get(0));//a 也是炸弹
+            return false;//a 不是炸弹
+        }
+        if(PokerTypeUtils.isBoom(a) != -1) return true;//a 是炸弹
+        if(a.size() != b.size()) return false;//已经排除了炸弹的可能，长度不相等,不能比较
+
+        PokerType aType = PokerTypeUtils.getType(a);
+        PokerType bType= PokerTypeUtils.getType(b);
+        if(aType == null || bType == null || aType.getType() != bType.getType()) return false;//牌型不相等不能比较
+        return aType.getSort() > bType.getSort();
+    }
+    /**
+     * 比较单牌的大小
+     */
+    private static boolean compareOne(Poker a,Poker b){
+        if(a.getSort() > b.getSort()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
